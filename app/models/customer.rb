775b89ca12a6658b67975894ef9c5f4172b20516
Alan <alandgoldroger@gmail.com>
2025-11-03 10:40:06 -0600

    Add initial customers via ActiveAdmin

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
 
   validates :full_name, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "id", "full_name", "email", "phone_number", "notes", "created_at", "updated_at" ]
+  end
 end
